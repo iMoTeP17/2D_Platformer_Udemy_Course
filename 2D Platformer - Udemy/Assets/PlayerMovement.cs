68ebf3ccd3b33f63c75a6e8f975145c021a6e639
unknown <imraanomar17@gmail.com>
2025-04-21 03:35:05 +0200

    Add wall slide and wall jump

--- 2D Platformer - Udemy/Assets/PlayerMovement.cs
+++ 2D Platformer - Udemy/Assets/PlayerMovement.cs
@@ -3,77 +3,186 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private static readonly int IsMoving = Animator.StringToHash("isMoving");
+    private static readonly int YVelocity = Animator.StringToHash("yVelocity");
+    private static readonly int Grounded = Animator.StringToHash("isGrounded");
+    private static readonly int IsWallSliding = Animator.StringToHash("isWallSliding");
+    private static readonly int IsWallDectected = Animator.StringToHash("isWallDectected");
 
-    // NOTE: public variables, if its private it doesnt get picked up in Unity editor
+    // NOTE: public variables, if its private it doesn't get picked up in Unity editor
     
-    private  Rigidbody2D rigidBody;
-    private Animator animator;
+    private  Rigidbody2D _rigidBody;
+    private Animator _animator;
     
     [Header("Movement Info")]
     public int moveSpeed;
-    private float movementInputX;
-    private float jumpForce = 16f;
-    private bool canDoubleJump;
+    private float _movementInputX;
+    private const float JumpForce = 16f;
+    //private bool _canDoubleJump;
+    private bool _canMove;
+    
     
     [Header("Ground Collision Info")]
     public LayerMask groundLayer;
     public Transform groundCheck;
-    
+    private const float GroundCheckRadius = 0.2f;
 
+
+    // Wall Detection
+    private bool _isWallDetected;
+    public  float wallCheckDistance;
+    
+    // Wall slide
+    private bool _isWallSliding;
+    
+    
+    
+    private bool _isFacingRight = true;
+    private int _facingDirection = 1;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        doMovement();
-    }
+        AnimationController();
+        FlipController();
+        WallCollisionCheck();
+        CanWallSlide();
+        CalculateMovementX();
 
-    private bool isGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        if (IsGrounded())
+        {
+            _canMove = true;
+        }
     }
-
 
     public void Move(InputAction.CallbackContext context)
     {
-        movementInputX = context.ReadValue<Vector2>().x;
+        _movementInputX = context.ReadValue<Vector2>().x;
+    }
+    
+    private void CalculateMovementX()
+    {
+        if (_canMove)
+            _rigidBody.linearVelocity = new Vector2(_movementInputX * moveSpeed, _rigidBody.linearVelocity.y);
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded())
+        if (context.performed && IsGrounded())
         {
-            rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, jumpForce);
-            canDoubleJump = true;
-
+            _rigidBody.linearVelocity = new Vector2(_rigidBody.linearVelocity.x, JumpForce);
+            //_canDoubleJump = true;
         }
         
         // Longer press jumps higher
-        if (context.canceled && rigidBody.linearVelocity.y > 0f)
+        if (context.canceled && _rigidBody.linearVelocity.y > 0f)
         {
-            rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, rigidBody.linearVelocity.y * 0.5f);
+            _rigidBody.linearVelocity = new Vector2(_rigidBody.linearVelocity.x, _rigidBody.linearVelocity.y * 0.5f);
         }
         
-        // Double Jump
-        if (context.performed && !isGrounded() && canDoubleJump)
+        // TODO add Double Jump, causes issues with wall jump
+        /*if (context.performed && !IsGrounded() && _canDoubleJump && !_isWallSliding)
         {
-            rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, jumpForce);
-            canDoubleJump = false;
+            _rigidBody.linearVelocity = new Vector2(_rigidBody.linearVelocity.x, JumpForce);
+            _canDoubleJump = false;
+            Debug.Log("I am DOUBLE JUMPING");
+        }*/
+        
+        // Wall Jump
+        if (context.performed && CanWallJump())
+        {
+            _canMove = false;
+            _isWallSliding = false;
+            _rigidBody.linearVelocity = new Vector2(7 * -_facingDirection, JumpForce+1f);
+        }
+    }
+    
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, GroundCheckRadius, groundLayer);
+    }
+
+    private void CanWallSlide()
+    {
+        if (_isWallDetected && IsCharacterFalling())
+        {
+            _isWallSliding = true;
+            _rigidBody.linearVelocity = new Vector2(0f, _rigidBody.linearVelocityY * 0.01f);
+        }
+        else if (!_isWallDetected)
+        {
+            _isWallSliding = false;
         }
     }
 
-    private void doMovement()
+    private bool CanWallJump()
     {
-        bool isMoving = rigidBody.linearVelocityX != 0f;
-        animator.SetBool("isMoving", isMoving);
-        
-        rigidBody.linearVelocity = new Vector2(movementInputX* moveSpeed, rigidBody.linearVelocity.y);
+        return _isWallSliding;
     }
     
+
+    private void WallCollisionCheck()
+    {       
+        _isWallDetected = Physics2D.Raycast(transform.position, Vector2.right * _facingDirection, wallCheckDistance, groundLayer);
+    }
+
+    private bool IsCharacterFalling()
+    {
+        return _rigidBody.linearVelocity.y < 0f;
+    }
+    
+
+    private void AnimationController()
+    {
+        // set we are moving left or right
+        bool isMoving = _rigidBody.linearVelocityX != 0f;
+        _animator.SetBool(IsMoving, isMoving);
+
+        // set we are moving in air
+        _animator.SetFloat(YVelocity, _rigidBody.linearVelocity.y);
+        
+        // set we are touching a platform
+        _animator.SetBool(Grounded, IsGrounded());
+        
+        // set we are sliding against a wall
+        _animator.SetBool(IsWallDectected, _isWallDetected);
+        _animator.SetBool(IsWallSliding, _isWallSliding);
+
+        
+    }
+
+    // Control Direction Character is facing
+    private void FlipController()
+    {
+        if (_isFacingRight && _rigidBody.linearVelocityX < 0f)
+        {
+            Flip();
+        }
+        else if (!_isFacingRight && _rigidBody.linearVelocityX > 0f)
+        {
+            Flip();
+        }
+    }
+    
+    private void Flip()
+    {
+        _facingDirection *= -1;
+        _isFacingRight = !_isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + wallCheckDistance * _facingDirection, transform.position.y));
+    }
 }
